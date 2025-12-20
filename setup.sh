f7e72189d5707fa780918471a0385f51846dd282
HatoriIchigo <hatori.ichigo.0802@gmail.com>
2025-12-20 11:14:09 +0900

    [REFACTOR] LazyGitインストールスクリプト追加

--- setup.sh
+++ setup.sh
@@ -67,11 +67,24 @@ function apt_installer() {
     fi
 
     # その他必要なツールのインストール
+    # ripgrep
     rg --version 2>&1 > /dev/null
     if [ $? -ne 0 ]; then
         echo "ripgrepをインストールします..."
         sudo apt-get update
         sudo apt-get install ripgrep -y
+    fi
+
+    # lazygit
+    lazygit --version 2>&1 > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "LazyGitをインストールします..."
+        LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+        curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+        tar xf lazygit.tar.gz lazygit
+        sudo install lazygit -D -t /usr/local/bin/
+        rm -rf lazygit
+        rm -rf lazygit.tar.gz
     fi
 
     # LSPインストール
