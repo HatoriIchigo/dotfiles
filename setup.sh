a1ad10dabcd497ba8757272247a56f31abeaec3b
HatoriIchigo <hatori.ichigo.0802@gmail.com>
2026-01-02 13:18:26 +0900

    [FEATURE] 代替ツール使用

--- setup.sh
+++ setup.sh
@@ -7,12 +7,75 @@ function npmInstall() {
     fi
 }
 
-###################################################
+function add_bashrc() {
+    if [ $(grep "$1" $HOME/.bashrc | wc -l) -eq 0 ]; then
+        echo "$1" >> $HOME/.bashrc
+    fi
+}
+
+# _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 #
 #           apt用インストールスクリプト
 #
-###################################################
+# _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 function apt_installer() {
+    ####################################################
+    #
+    #               代替ツールインストール
+    #
+    ####################################################
+    # fzfインストール
+    fzf --version 2>&1 > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "fzfをインストールします..."
+        sudo apt-get update
+        sudo apt-get install -y fzf
+    fi
+
+    # bat (cat代替ツール)
+    batcat --version 2>&1 > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "batをインストールします..."
+        sudo apt-get update
+        sudo apt-get install -y bat
+    fi
+    add_bashrc "alias cat='batcat'"
+
+    # zoxide (cd代替ツール)
+    # zoxide --version 2>&1 > /dev/null
+    # if [ $? -ne 0 ]; then
+    #     echo "zoxideをインストールします..."
+    #     curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+    # fi
+    # add_bashrc "alias cd='zoxide'"
+
+    # eza (ls代替ツール)
+    eza --version 2>&1 > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "ezaをインストールします..."
+        sudo mkdir -p /etc/apt/keyrings
+        wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+        echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+        sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+        sudo apt update
+        sudo apt install -y eza
+    fi
+    add_bashrc "alias ls='eza --icons --group-directories-first --color=auto'"
+
+    # ripgrep (grep代替ツール)
+    rg --version 2>&1 > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "ripgrepをインストールします..."
+        sudo apt-get update
+        sudo apt-get install ripgrep -y
+    fi
+    add_bashrc "alias grep='rg'"
+
+    ####################################################
+    #
+    #          neovim関連ツールインストール
+    #
+    ####################################################
     # neovimインストール
     nvim --version 2>&1 > /dev/null
     if [ $? -ne 0 ]; then
@@ -66,14 +129,6 @@ function apt_installer() {
     fi
 
     # その他必要なツールのインストール
-    # ripgrep
-    rg --version 2>&1 > /dev/null
-    if [ $? -ne 0 ]; then
-        echo "ripgrepをインストールします..."
-        sudo apt-get update
-        sudo apt-get install ripgrep -y
-    fi
-
     # lazygit
     lazygit --version 2>&1 > /dev/null
     if [ $? -ne 0 ]; then
